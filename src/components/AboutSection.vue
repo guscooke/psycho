665efdac8b96df8e6118fb29b15cb5de70c905ce
Gustavo <guscooke14@gmail.com>
2021-05-17 19:59:20 -0300

    commit 5

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -3,17 +3,18 @@
     <v-container fluid>
       <v-row align="center" justify="center">
         <v-col cols="10">
-          <v-row align="center" justify="center">
-            <v-col cols="12" md="7">
-              <!-- <h1 class="font-weight-light display-2">la balb</h1> -->
-              <h1 class="pa-6 font-weight display-1 mb-3" justify="center">
+             <h1 class="pa-6 font-weight display-1 mb-3 mt-6" justify="center">
                 Entenda como funciona!
               </h1>
-              <h3 class="font-weight" justify="center">No nosso serviço de telemedicina, você pode realizar a sua consulta com a
+              <h3 class="font-weight mb-4" justify="center">No nosso serviço de telemedicina, você pode realizar a sua consulta com a
                 especialidade desejada via
                 chamada de vídeo. </h3>
               <br>
-              <h3 class="pa-6" justify="center">Para agendar a sua primeira consulta é bem simples: </h3>
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <!-- <h1 class="font-weight-light display-2">la balb</h1> -->
+           
+              <h3 class="pa-8" justify="center">Para agendar a sua primeira consulta é bem simples: </h3>
               <v-row>
                 <v-col cols="12" class="pa-6 d-flex align-center">
                   <v-img src="@/assets/img/one.png" max-width="60px" class="mr-4" />
