6fb54239d17d9a4986c49bb4dd5b096b8872663e
Gustavo <guscooke14@gmail.com>
2021-06-25 09:03:57 -0300

    teste 3

--- src/components/QuemSomos.vue
+++ src/components/QuemSomos.vue
@@ -42,7 +42,7 @@
 
 <style scoped>
 #download {
-  background-image: url("~@/assets/img/bgx.jpg");
+  background-image: url("~@/assets/img/bgx.jpg") !important;
   background-attachment: fixed;
   background-repeat: no-repeat;
   /* background-size: cover; */
